96bf86ca60aa7f8c39aa4a72d496f7c21fd18dfa
João Lima <cyberze@hotmail.com>
2021-06-11 06:13:42 +0100

    GetUpgrades

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var instance_skel = require('../../instance_skel');
 var OSC = require('osc');
 var stripDef = require('./stripdef.json');
@@ -51,10 +50,23 @@ function instance(system, id, config) {
 		self.config._configIdx = -1;
 	}
 
-	self.addUpgradeScript(function(config, actions, releaseActions, feedbacks) {
-		var changed = false;
+	// each instance needs a separate local port
+	id.split('').forEach(function (c) {
+		po += c.charCodeAt(0);
+	});
+	self.port_offset = po;
 
-		function upgradePass(actions, changed) {
+	self.debug = debug;
+	self.log = log;
+
+	return self;
+}
+
+instance.GetUpgradeScripts = function() {
+	return [
+		function(context, config, actions, feedbacks) {
+			var changed = false;
+	
 			for (var k in actions) {
 				var action = actions[k];
 
@@ -73,25 +85,9 @@ function instance(system, id, config) {
 			}
 			return changed;
 		}
-
-		changed = upgradePass(actions, changed);
-		changed = upgradePass(releaseActions, changed);
-
-
-		return changed;
-	});
-
-	// each instance needs a separate local port
-	id.split('').forEach(function (c) {
-		po += c.charCodeAt(0);
-	});
-	self.port_offset = po;
-
-	self.debug = debug;
-	self.log = log;
-
-	return self;
+	]
 }
+
 
 instance.prototype.ICON_SOLO =
 	'iVBORw0KGgoAAAANSUhEUgAAAEgAAAA6CAYAAAATBx+NAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAUcSURBVHic7ZpLaFxVGIC//96ZTNKkqSnGN+JzUS3oyqi1ogstqKAIuhIVFEUUtOBGhC5UxI2FgigYKLqpFRdV8FFsN1rFbtoGWwyl1lStsa+0NjNJzsy5M7+LuXdyZzKT00zSJpTzwYU573O/+c859w4jeQ0UTytMsNgzWOp4QQ4y6YQijyrl4cWazFIgIHgHeDJJ1wkKKf/dLRy64LNaQuSV8XTaLzEHXpADL8iBF+TAC3LgBTnwghx4QQ68IAdekAMvyIEX5MALcuAFOfCCHHhBDrwgB16QAy/IgRfkwAty4AU58IIceEEOvCAHXpADL8iBF+TAC3LgBTnwghx4QQ68IAcNf8GjfzEmoUpfucgalJUIY2VhpKODYRHa+gduZPhY4XpVjnd08dS8JpfXQJNrXIORgnL5vDqcIyXDC9ZQsAZtuE5Ehofa6dMafrUGLRlG5to2r8FgyslUbYmJgsB1SrBzXLm0nYnNldIkAwIfAd1NivsryhUXYh6zURPUYStINaBXC8GOs8rK8z54wLOpOWxEuVfgEYQ3YYn8mTQJpzgkdaJcC699/yl953Nsa9gRL6eTjWWqBKqsaMjrskXesIY91jBqDfut4T1tmGerJaZKr53i7bh81BqGbJENqtMR3LjE6gTNlBT+clJZvtBiUjeyLR43iqZ4WpVsq7qqdFjDj032KrWG31S5JNXvDEGqLLeGoabti+xWpbOZoBnHvABZWyGoKKB3dhJuP6H0LKya2mB74k+hCp9GJf61hi+iIk+okktXjYq8CqyN5/g7yrvAUFy8qlzkrdmGiopsAG6Lkwfi9gcBUAaiEq83bZjXQAupCEpfE2VJImnXMW26kc4LVfoiw+EWUbHfGG5I6iZRYQ1lY7gxbr/CGsbj/NOq1f2sWQRZw7G43pSOVw8hneQaa7Bx/sHYx+wRlKbDKmE1ku7pRrYlYbhQiHAmLHEHsAk401C8OqzmEy+9W+P84c5ODsftzwI/xfl9xnBts3F0giuh9viyW3o5BSDLOIrWovBmbRIEVUGzPI5la5LkgQLyZWPozxfpZSzbyWuZHJeh3CfC5lTxOlVCoIfp024s3V6lerMAYVi/qScUQ3pTyVN1hVLrT5isqwec46tG1iphWQFZV0C2zraZtosIUbaLHzI5ngN2JUMzQT8wwfTXWHdiSeoEq1TIN+s7V6GQStafzOkTcNnM9uf8LpaNapIeiyVlnI0cWMPGkuFlVTpq863KTx5UK3QzLkKJZEOFW3SSq+O6XcCaOH88l+PPpgN1MZqKlAGNT2bN049wO4DAiEidSGCOL6spSY8XCLbMV5IqVwl8EBX5xxq+iopsjooMAaviKvtEmKR6B1vivDAK+LpkWB8V+Y44IgQ+F6HcbBwRVJTP4mRPVGJ7ybA+yvAtVL8c1Vr/9eQ10EKl+SnW6pqMJHl3+yQ5OdqhNMXWWcYpR4aHUzKXWcPeZnVLhiNamH6HbPEctDIyHGox1oEkquZ0irUiiSSBZyYIBtuVlLW8ovAS8L3AH0CJ6mm2A+XBTCffJHVFmMzkuB94X+EIYIFRgcFsmbukh+O1urAb2Inyc6r96dByt8CHwFHAKvwFbMrkWCvSfP+SvAYqCrlSZc43GGWEKBSAwR4qL7b788RSIq/BIPB8nDTzEgQQhUKUEUCHQfYu1EQXkQHgpvjztKDqq0V7VAJBZUEmt9QwGQAVKIcX5x3Ol4xUH8LaRqt9CNVN86JCYep/T6xGm2u0hEsAAAAASUVORK5CYII=';
@@ -1274,6 +1270,10 @@ instance.prototype.init_strips = function () {
 						label: stripDef[i].description + " " + c + " %",
 						name: fID + "_p"
 					});
+                        defVariables.push({
+                        label: stripDef[i].description + " " + c + " Mute",
+                        name: fID + "_m"
+                    });
 					if (stripDef[i].hasLevel) {
 						for (b = 1; b<11; b++) {
 							bOrF = (b < 7 ? 'b' : 'f');
@@ -1515,7 +1515,13 @@ instance.prototype.init_osc = function() {
 				case 'on':
 					self.xStat[node].isOn = (v == 1);
 					self.checkFeedbacks(self.xStat[node].fbID);
-					break;
+                                            fds = node.split('/');
+                        if (fds[2] < 10 && fds[2] > 0) {
+                            crl = fds[2].substring(1);
+                            self.setVariable('f_ch' + crl + '_m', v);
+                        } else if (fds[2] >= 10 && fds[2] <= 16) {
+                            self.setVariable('f_ch' + fds[2] + '_m', v);
+                        }					break;
 				case '1':
 				case '2':
 				case '3':
